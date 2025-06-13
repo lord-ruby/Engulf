b62da1cf4591b21bc5a425761bac71d9b6eb1791
Ruby <darkdragongaming379@gmail.com>
2025-06-13 14:11:19 +0100

    update edition detection

--- Engulf.lua
+++ Engulf.lua
@@ -35,9 +35,30 @@ Engulf.EditionFuncs = {
     e_holo = Engulf.Editionfunc({mult=true,type="+"}, "multhit1", function(edition) return edition.mult end),
     e_polychrome = Engulf.Editionfunc({mult=true,type="X"}, "multhit2", function(edition) return edition.x_mult end),
 }
+local level_up_handref = level_up_hand
+function level_up_hand(card, hand, instant, amount,...)
+    level_up_handref(card, hand, instant, amount,...)
+    if card and card.edition and to_big(amount or 1) > to_big(0) then
+        if Engulf.SpecialFuncs[card.config.center.key] then 
+        else Engulf.EditionHand(card, hand, card.edition, math.min(amount or 1, Engulf.config.stackeffects and 1 or amount), instant) end end
+end
 Engulf.SpecialWhitelist = {}
 function Engulf.EditionHand(card, hand, edition, amount, instant)
-    if Engulf.EditionFuncs[edition.key] then Engulf.EditionFuncs[edition.key](card, hand, instant, Engulf.config.stackeffects and amount or 1, edition) end
+    if Engulf.EditionFuncs[edition.key] then Engulf.EditionFuncs[edition.key](card, hand, instant, Engulf.config.stackeffects and amount or 1, edition) else
+        if edition.chips then Engulf.Editionfunc({chips=true,type="+"}, "chips1", function(edition) return edition.chips end)(card, hand, instant, amount or 1, edition) end
+        if edition.mult then Engulf.Editionfunc({mult=true,type="+"}, "multhit1", function(edition) return edition.mult end)(card, hand, instant, amount or 1, edition) end
+        if edition.x_chips then Engulf.Editionfunc({chips=true,type="X"}, "talisman_xchip", function(edition) return edition.x_chips end)(card, hand, instant, amount or 1, edition) end
+        if edition.x_mult then Engulf.Editionfunc({mult=true,type="X"}, "multhit2", function(edition) return edition.x_mult end)(card, hand, instant, amount or 1, edition) end
+        if edition.e_chips then Engulf.Editionfunc({chips=true,type="^"}, "talisman_echip", function(edition) return edition.e_chips end)(card, hand, instant or 1, amount, edition) end
+        if edition.e_mult then Engulf.Editionfunc({mult=true,type="^"}, "talisman_emult", function(edition) return edition.e_mult end)(card, hand, instant, amount or 1, edition) end
+        if edition.ee_chips then Engulf.Editionfunc({chips=true,type=2}, "talisman_eechip", function(edition) return edition.ee_chips end)(card, hand, instant, amount or 1, edition) end
+        if edition.ee_mult then Engulf.Editionfunc({mult=true,type=2}, "talisman_eemult", function(edition) return edition.ee_mult end)(card, hand, instant, amount or 1, edition) end
+        if edition.eee_chips then Engulf.Editionfunc({chips=true,type=3}, "talisman_eeechip", function(edition) return edition.eee_chips end)(card, hand, instant, amount or 1, edition) end
+        if edition.eee_mult then Engulf.Editionfunc({mult=true,type=3}, "talisman_eeemult", function(edition) return edition.eee_mult end)(card, hand, instant, amount or 1, edition) end
+        if edition.hyper_chips then Engulf.Editionfunc({chips=true,type=edition.hyper_chips[1]}, "talisman_eeechip", function(edition) return edition.hyper_chips[2] end)(card, hand, instant, amount or 1, edition) end
+        if edition.hyper_mult then Engulf.Editionfunc({mult=true,type=edition.hyper_chips[1]}, "talisman_eeemult", function(edition) return edition.hyper_chips[2] end)(card, hand, instant, amount or 1, edition) end
+        if edition.repetitions then level_up_hand(card, hand, instant, edition.repetitions) end
+    end
 end
 
 function Engulf.performop(num1, num2, op)
@@ -46,14 +67,7 @@ function Engulf.performop(num1, num2, op)
     if op == "^" then return to_big(num1)^to_big(num2) end
     if type(op) == "number" then to_big(num1):arrow(op, num2) end
 end
-function Engulf.StackOP(num1, num2, op) if op == "^" or op == "X" then return to_big(num1)^to_big(num2) else return to_big(num1)*to_big(num2) end end
-local level_up_handref = level_up_hand
-function level_up_hand(card, hand, instant, amount,...)
-    level_up_handref(card, hand, instant, amount,...)
-    if card and card.edition and to_big(amount or 1) > to_big(0) then
-        if Engulf.SpecialFuncs[card.config.center.key] then 
-        else Engulf.EditionHand(card, hand, card.edition, amount, instant) end end
-end
+function Engulf.StackOP(num1, num2, op) if op == "^" or op == "X" or type(op) == "number" then return to_big(num1)^to_big(num2) else return to_big(num1)*to_big(num2) end end
 local use_cardref = G.FUNCS.use_card
 G.FUNCS.use_card = function(e, mute, nosave,...)
     local card = e.config.ref_table
